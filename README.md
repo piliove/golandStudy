c499b8c70e3e20e7f4babfc17d82d183b7dd7e2f
piliove <1529578681@qq.com>
2023-01-30 11:10:23 +0800

    Go基本数据结构的使用:string、slice、map

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # golandStudy
-golandStudy train
+主要内容：golandStudy train
+编辑者：ljz
 
 #练习 goland基础语法以及相关的一些库和框架方面
+
+
--- src/Test/six.go
+++ src/Test/six.go
@@ -8,6 +8,12 @@ func main() {
 	//声明方式二
 	ints2 := make([]int, 0)
 
+	// fmt.Println(ints1)
+	// fmt.Println(ints2)
+
+	ints1 = append(ints1,1,2,3,4,5)
+	ints2 = append(ints2,1,2,3,4,5,6,7)
+
 	fmt.Println(ints1)
 	fmt.Println(ints2)
 }
