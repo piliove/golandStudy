b5aad94887199f0f5a0535f3b4f8479a435b5a4f
piliove <1529578681@qq.com>
2023-01-31 21:01:58 +0800

    Go基本数据结构的使用:string、slice、map

--- src/Test/eight.go
+++ src/Test/eight.go
@@ -7,15 +7,25 @@ import (
 
 type ProductInfo struct {
 	Name  string  `json:"name"`
-	Price float32 `json:"price"`
+	Price string `json:"price"`
 }
 
 func main() {
-	fmt.Println("111")
+
+	str := "{\"name\":\"AppleWatchS8\",\"price\":\"3199\"}"
+	data := ProductInfo{}
+
+	err := json.Unmarshal([]byte(str), &data)
+	fmt.Println(str)
+	// fmt.Println(data)
+	fmt.Println(err)
+
+	name := ProductInfo{}.name
+	fmt.Println(name)
+	fmt.Printf("%+v", data)
+
 	return
 
-	str := "{'name':'AppleWatchS8','price':'3199'}"
-	data := ProductInfo{}
 	if err := json.Unmarshal([]byte(str), &data); err != nil {
 		fmt.Println("error: " + err.Error())
 	} else {
