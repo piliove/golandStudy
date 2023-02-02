2fc5a3c522942929f93c38fa928034b982c93447
piliove <1529578681@qq.com>
2023-02-02 17:07:53 +0800

    Go基本使用语法

--- src/Test/nine.go
+++ src/Test/nine.go
@@ -1,11 +1,30 @@
 package main
 
 import (
-"fmt"
+	"fmt"
+	"reflect"
 )
 
+type Person struct {
+	Name string `json:name`
+	Age int `json:age`
+}
+
 func main() {
-	m := map[int]string{1: "a", 2: "b", 3: "c"}
-	len := len(m)
-	fmt.Println(len)
+	// m := map[int]string{1: "a", 2: "b", 3: "c"}
+
+	m := Person{
+		Name: "HaiCoder",
+		Age:109,
+	}
+
+	// len := len(m)
+	// fmt.Println(len)
+
+	fmt.Println(m)
+
+	personValue := reflect.ValueOf(m)
+	nameValue := personValue.FieldByName("Name").String()
+
+	fmt.Println(nameValue)
 }
